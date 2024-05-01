f3f651ea72087d3a49b67cb7be7f6fb1a21e580b
prathmeshbhujbal711 <prarhmeshbhujba7111@gmail.com>
2024-05-01 22:52:47 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,4 @@
+print("Hello World")
+print("Google App Engine")
+print("This Python Document is deployed with the help of Google Cloud Platform which is cloned via github repository.")
+print("Thank You !")
